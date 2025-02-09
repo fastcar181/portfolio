764d3e3879175ca248d683febbb23eda7e50603c
fastcar181 <191656932+fastcar181@users.noreply.github.com>
2025-02-08 23:35:24 -0500

    Created the basic portfolio structure

---
+++ app/projects/page.js
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-3xl font-bold">Projects Page</h1>
+      </div>
+    );
+  }
+  
